f1b32e1bc34974b8663de8404cddc44305852b0a
andrushko1 <sasha1f@protonmail.com>
2022-07-16 11:58:10 +0300

    initial commit

---
+++ main.rs
@@ -0,0 +1,17 @@
+fn main {
+    const SIZE: usize = 100_000;
+    const N_ARRAY: usize = 1_000_000;
+
+    recursive_func(N_ARRAY);
+}
+
+fn create_array() -> [u8; SIZE] {
+    [0u8; SIZE]
+}
+
+fn recursive_func(n: usize) {
+    let a = create_array();
+    println!("{} {}", N_ARRAY - n + 1, a[0]);
+    if n > 1 { recursive_func(n - 1) }
+}
+
